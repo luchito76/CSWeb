583da2141cb4f82bcfc0b030964a735d7fe00ccc
luchto76 <luisparadawagner@gmail.com>
2014-04-11 11:55:03 -0300

    Se subio el Logout

---
+++ CuotaSystem/Logout.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CuotaSystem
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            logOut();
+        }
+
+        private void logOut()
+        {
+            Session.Clear();
+            Response.Redirect("Login.aspx");
+        }
+
+    }
+}
